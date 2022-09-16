6e972e01d91976894754edb31dde49893bf8bbeb
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2022-09-15 21:15:38 -0500

    feat(*): add localstorage functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Formulario from './components/Formulario'
 import { Header } from './components/Header'
 import ListadoPacientes from './components/ListadoPacientes'
@@ -6,6 +6,16 @@ import ListadoPacientes from './components/ListadoPacientes'
 const App = () => {
   const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState({});
+
+  useEffect(() => {
+    const pacientesLocal = JSON.parse(localStorage.getItem('pacientes'));
+    pacientesLocal?.length > 0 && setPacientes(pacientesLocal);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify( pacientes ));
+  }, [pacientes]);
+
 
   const eliminarPaciente = (id) => {
     const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id)
